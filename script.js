611ba682218f150e5a1cbe51e8e2e89bbb572b21
Spider x <ankushpatel3727@gmail.com>
2025-06-28 21:42:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,115 +1,68 @@
- document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const tvDiv = document.querySelector('.tv');
 
-    const tvDiv = document.querySelector('.tv');
+  const images = [
+    'https://picsum.photos/300/200?random=1',
+    'https://picsum.photos/300/200?random=2',
+    'https://picsum.photos/300/200?random=3',
+    'https://picsum.photos/300/200?random=4',
+    'https://picsum.photos/300/200?random=5',
+    'https://source.unsplash.com/300x200/?netflix',
+    'https://source.unsplash.com/300x200/?series',
+    'https://source.unsplash.com/300x200/?tv-show',
+    'https://source.unsplash.com/300x200/?amazon-prime',
+    'https://source.unsplash.com/300x200/?hbo',
+    'https://source.unsplash.com/300x200/?disney',
+    'https://source.unsplash.com/300x200/?thriller',
+    'https://source.unsplash.com/300x200/?drama',
+    'https://source.unsplash.com/300x200/?comedy',
+    'https://source.unsplash.com/300x200/?action',
+    'https://source.unsplash.com/300x200/?movie',
+    'https://source.unsplash.com/300x200/?hollywood',
+    'https://source.unsplash.com/300x200/?bollywood',
+    'https://source.unsplash.com/300x200/?cinema',
+    'https://source.unsplash.com/300x200/?film',
+    'https://source.unsplash.com/300x200/?actor',
+    'https://source.unsplash.com/300x200/?director',
+    'https://source.unsplash.com/300x200/?scene',
+    'https://source.unsplash.com/300x200/?poster',
+    'https://source.unsplash.com/300x200/?marvel',
+    'https://source.unsplash.com/300x200/?sports',
+    'https://source.unsplash.com/300x200/?football',
+    'https://source.unsplash.com/300x200/?cricket',
+    'https://source.unsplash.com/300x200/?tennis',
+    'https://source.unsplash.com/300x200/?basketball',
+    'https://source.unsplash.com/300x200/?badminton',
+    'https://source.unsplash.com/300x200/?volleyball',
+    'https://source.unsplash.com/300x200/?hockey',
+    'https://source.unsplash.com/300x200/?cycling',
+    'https://source.unsplash.com/300x200/?swimming'
+  ];
 
-    // Array of movie-related images (you can replace these with your own URLs)
+  let currentIndex = 0;
+  let imgFade;
 
-    const images = [
+  function setImageWithFade(index) {
+    if (!tvDiv) return;
 
-        'https://picsum.photos/300/200?random=1',
+    const newImage = new Image();
+    newImage.src = images[index];
 
-        'https://picsum.photos/300/200?random=2',
+    newImage.onload = () => {
+      tvDiv.style.opacity = 0;
+      clearTimeout(imgFade);
 
-        'https://picsum.photos/300/200?random=3',
+      imgFade = setTimeout(() => {
+        tvDiv.style.backgroundImage = `url('${newImage.src}')`;
+        tvDiv.style.opacity = 1;
+      }, 500);
+    };
+  }
 
-        'https://picsum.photos/300/200?random=4',
+  setImageWithFade(currentIndex);
 
-        'https://picsum.photos/300/200?random=5',
-
-        "https://source.unsplash.com/300x200/?netflix",
-
-        "https://source.unsplash.com/300x200/?series",
-
-        "https://source.unsplash.com/300x200/?tv-show",
-
-        "https://source.unsplash.com/300x200/?amazon-prime",
-
-        "https://source.unsplash.com/300x200/?hbo",
-
-        "https://source.unsplash.com/300x200/?disney",
-
-        "https://source.unsplash.com/300x200/?thriller",
-
-        "https://source.unsplash.com/300x200/?drama",
-
-        "https://source.unsplash.com/300x200/?comedy",
-
-        "https://source.unsplash.com/300x200/?action",
-
-        "https://source.unsplash.com/300x200/?movie",
-
-        "https://source.unsplash.com/300x200/?hollywood",
-
-        "https://source.unsplash.com/300x200/?bollywood",
-
-        "https://source.unsplash.com/300x200/?cinema",
-
-        "https://source.unsplash.com/300x200/?film",
-
-        "https://source.unsplash.com/300x200/?actor",
-
-        "https://source.unsplash.com/300x200/?director",
-
-        "https://source.unsplash.com/300x200/?scene",
-
-        "https://source.unsplash.com/300x200/?poster",
-
-        "https://source.unsplash.com/300x200/?marvel",
-
-        "https://source.unsplash.com/300x200/?sports",
-
-        "https://source.unsplash.com/300x200/?football",
-
-        "https://source.unsplash.com/300x200/?cricket",
-
-        "https://source.unsplash.com/300x200/?tennis",
-
-        "https://source.unsplash.com/300x200/?basketball",
-
-        "https://source.unsplash.com/300x200/?badminton",
-
-        "https://source.unsplash.com/300x200/?volleyball",
-
-        "https://source.unsplash.com/300x200/?hockey",
-
-        "https://source.unsplash.com/300x200/?cycling",
-
-        "https://source.unsplash.com/300x200/?swimming"
-
-    ];
-
-    function setRandomImage() {
-
-        const randomIndex = Math.floor(Math.random() * images.length);
-
-        const randomImage = images[randomIndex];
-
-        // Create an image element to preload the image
-
-        const img = new Image();
-
-        img.src = randomImage;
-
-        img.onload = () => {
-
-            tvDiv.style.backgroundImage = `url('${randomImage}')`;
-
-            tvDiv.style.backgroundSize = 'cover';
-
-            tvDiv.style.backgroundPosition = 'center';
-
-        };
-
-    }
-
-    
-
-    // Set an initial image
-
-    setRandomImage();
-
-    // Change the image every 5 seconds
-
-    setInterval(setRandomImage, 3000);
-
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    setImageWithFade(currentIndex);
+  }, 3000);
 });
